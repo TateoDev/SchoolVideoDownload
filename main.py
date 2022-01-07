962aa16bb80f8d7c1bbaf7fdd8e63a43f8f5f785
Tateo <91488652+TateoDev@users.noreply.github.com>
2022-01-07 09:48:40 -0700

    Update main.py
    Added support to download as mp4 or mp3.

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from pytube import YouTube
+import os
+import moviepy.editor as mp
+
+
+def download_mp4(video):
+  print("downloading video, this may take a bit")
+  try:
+    yt = YouTube(video)
+    yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first().download(os.getcwd(), filename="output.mp4")
+
+    print("video properly downloaded")
+  except:
+    print("something went wrong")
+
+def download_mp3(video):
+  print("downloading video, this may take a bit")
+  try:
+    yt = YouTube(video)
+    yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first().download(os.getcwd(), filename="output.mp4")
+
+    video = mp.VideoFileClip(r"output.mp4")
+    video.audio.write_audiofile(r"output.mp3")
+
+
+
+    print("video properly downloaded")
+  except:
+    print("something went wrong")
+
 
 video = str(input("Enter the URL of the video you would like to download \n"))
 
-print("downloading video, this may take a bit")
-try:
-  YouTube(video).streams.first().download()
-  print("video properly downloaded")
-except:
-  print("something went wrong")
+choice = input("would you like to download an mp4 or mp3?\n")
+
+if choice.lower() == "mp4":
+  download_mp4(video)
+elif choice.lower() == "mp3":
+  download_mp3(video)
+else:
+  print("enter a valid option")
+
